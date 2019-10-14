5881b113c1c37a09dd4165eba3a880cc1cbab53c
Andrei <st.vooly@gmail.com>
2019-10-14 20:21:28 +0300

    initial commit

---
+++ src/test/java/by/hryharenka/ots/bot/BotApplicationTests.java
@@ -0,0 +1,16 @@
+package by.hryharenka.ots.bot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BotApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
